b43520569f0346ff845dc9be715f4e892293774a
shahryar haghighi fard <shahryarhaghighifard@shahryars-MacBook-Pro.local>
2021-02-27 12:48:04 -0500

    added genre

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-import reactDom from "react-dom";
-import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./pagination";
+import { getMovies } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    genres: getGenres(),
+    pageSize: 4,
+    currentPage: 1,
   };
 
   handleDelete = (movie) => {
@@ -28,52 +33,65 @@ class Movies extends Component {
     return this.state.movies[0];
   };
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
-    if (this.state.movies.length === 0)
-      return <h2>There are no available movies!</h2>;
-    else {
-      return (
-        <React.Fragment>
-          <h2>There are {this.state.movies.length} available movies!</h2>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Genre</th>
-                <th scope="col">Stock</th>
-                <th scope="col">Rate</th>
-                <th scope="col">Like</th>
-                <th scope="col">Delete</th>
+    const { length: count } = this.state.movies;
+    const { currentPage, pageSize, movies: allmovies, genres } = this.state;
+    if (count === 0) return <h2>There are no available movies!</h2>;
+
+    // const filteredMovies = filterMovies(allmovies);
+    const movies = paginate(allmovies, currentPage, pageSize);
+
+    return (
+      <React.Fragment>
+        <h2>There are {this.state.movies.length} available movies!</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col">Like</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie.id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {this.state.movies.map((movie) => (
-                <tr key={movie.id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      liked={movie.liked}
-                      onClick={() => this.handleLike(movie)}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </React.Fragment>
-      );
-    }
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
+    );
   }
 }
 
